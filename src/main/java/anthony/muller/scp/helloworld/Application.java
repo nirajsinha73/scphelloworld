0b27409dd7a0c4053e8542b1e6de3cb765bad7a1
i051610 <anthony.muller@sap.com>
2017-10-09 11:19:01 +0000

    first commit - very basic spring boot application running on CF

---
+++ src/main/java/anthony/muller/scp/helloworld/Application.java
@@ -0,0 +1,9 @@
+package anthony.muller.scp.helloworld;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+}
